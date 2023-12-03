66ad854d760c0698744815d0be9bfd7715b15f54
hola-aryan <aryanisastudent@gmail.com>
2023-12-03 19:49:23 +0530

    App using SQL

---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const db = require('./util/database');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// (async () => {
+//   try {
+//     const result = await db.execute('SELECT * FROM products');
+//     console.log(result[0],result[1]);
+//   } catch (error) {
+//     console.error('Error executing database query:', error);
+//   }
+// })();
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Aryan@123'
+});
+
+module.exports = pool.promise();
