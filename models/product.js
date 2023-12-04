f6667eab7f56866f6bc74ab5826264104dd19e4f
hola-aryan <aryanisastudent@gmail.com>
2023-12-05 01:36:10 +0530

    Sequelize initials

--- models/product.js
+++ models/product.js
@@ -1,30 +1,27 @@
-const db = require ('../util/database')
+const Sequelize = require ('sequelize');
 
-const Cart = require('./cart');
+const sequelize = require('../util/database');
 
+const Product = sequelize.define('product', {
+  id:{
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull:false,
+    primaryKey:true
+  },
+  title : Sequelize.STRING,
+  price:{
+    type: Sequelize.DOUBLE,
+    allowNull:false
+  },
+  imageUrl:{
+    type: Sequelize.STRING,
+    allowNull:false
+  },
+  description:{
+    type: Sequelize.STRING,
+    allowNull:false
+  },
+});
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save() {
-   return db.execute("INSERT INTO products (title, price, description, image_url) VALUES (?,?,?,?);", [this.title,this.price,this.description, this.imageUrl]);
-  }
-
-  static deleteById(id) {
-    return db.execute("DELETE * FROM products WHERE products.id = ?",[id]);
-  }
-
-  static fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
-
-  static findById(id) {
-    return db.execute("SELECT * FROM products WHERE products.id = ?",[id]);
-  }
-};
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,10 +1,17 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Aryan@123'
-});
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'Aryan@123'
+// });
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root','Aryan@123', {dialect:'mysql',
+ host:'localhost'});
+
+ module.exports = sequelize;
